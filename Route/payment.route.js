4a9b6319198c67fb85d2560c0d7b5df86e4b066b
AnuragEnnippissery <anuragramesh608@gmail.com>
2025-12-09 22:07:46 +0530

    added middleware to payment routes

--- Route/payment.route.js
+++ Route/payment.route.js
@@ -1,11 +1,12 @@
 import { GetPaymentsByUser,InsertPayment, PaymentSummary,
     GetDairy,GetVegetable,GetSubCategory } from "../Controller/payment.controller.js";
+import { AuthenticateUser } from "../Controller/user.controller.js";
 
 export default function PaymentRoutes(app){
     app.post("/api/payment/add",InsertPayment),
     app.get("/api/payment/user/:userId",GetPaymentsByUser)
-    app.get("/api/payment/user/summary/:userId",PaymentSummary)
+    app.get("/api/payment/user/summary/:userId",AuthenticateUser,PaymentSummary)
     app.get("/api/payment/dairy/:userId",GetDairy)
     app.get("/api/payment/vegetable/:userId",GetVegetable)
-    app.get("/api/payment/:userId/:sub_category",GetSubCategory)
+    app.get("/api/payment/:userId/:sub_category",AuthenticateUser,GetSubCategory)
 }
